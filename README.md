c485b14d82743a02d0b950949f6acabf54633403
mykeoye <mikeysweede@gmail.com>
2023-06-29 01:04:36 +0100

    Made a simple project documentation about this work

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# crypto-rs
-A curation of solutions to all programming assignments in the Cryptography I course
+# Cryptolite
+An implementation of popular cryptographic and hashing primitives. This is a purely educative effort aimed at learning how
+these primitives work. Heavily inspired by the problem sets in the course [Cryptography I](https://www.coursera.org/learn/crypto)
+
+## License
+This project is licensed under the GNU Genearl Public License Version 3 - see the [LICENSE](LICENSE) file for details
--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,36 @@ use crypto::{
 
 const AES_BLOCK_SIZE: usize = 16;
 
+/// Encrypts bytes of plaintext using AES in counter based chaining mode (CBC)
+/// the final output contains the ciphertext with the IV prepended. This implementation
+/// uses PKCS#5 padding, as padding is requirement in implementing cbc mode. Its important
+/// to note that the plaintext and key should be passed as unencoded bytes.
 pub fn aes_128_cbc_encrypt(plaintext: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
+    // Pad the plaintext with the required number of pad bytes before encryption
     let ptext_bytes = pkcs5_pad(plaintext);
+
     let mut prev_block = iv.to_vec();
     let mut ciphertext = vec![];
 
     let cipher = AesSafe128Encryptor::new(key);
     let mut encrypted_block = [0; AES_BLOCK_SIZE];
+
     for block in ptext_bytes.chunks_exact(AES_BLOCK_SIZE) {
         cipher.encrypt_block(&xor(&prev_block, block), &mut encrypted_block);
         ciphertext.extend_from_slice(&mut encrypted_block);
         prev_block = encrypted_block.to_vec();
     }
+
+    // Prepend the iv to the fully generated cipher text, this ensures decryption can extract the iv
     let mut ciphertext_with_iv = iv.to_vec();
     ciphertext_with_iv.extend_from_slice(&ciphertext);
     ciphertext_with_iv
 }
 
+/// Decrypts bytes of ciphertext using the cipher AES in couter based chaining mode (CBC)
+/// the final output removes the padding applied during the encryption process. The ciphertext,
+/// bytes should be passed with the prepended IV. This implementation seeks the IV by extracting
+/// the first 16bytes.
 pub fn aes_128_cbc_decrypt(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
     let cipher = AesSafe128Decryptor::new(key);
 
@@ -32,31 +45,44 @@ pub fn aes_128_cbc_decrypt(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
     let mut plaintext = vec![];
 
     let mut decrypted_block = [0; AES_BLOCK_SIZE];
-    for block in ciphertext[AES_BLOCK_SIZE..].chunks(AES_BLOCK_SIZE) {
+    for block in ciphertext[AES_BLOCK_SIZE..].chunks_exact(AES_BLOCK_SIZE) {
         cipher.decrypt_block(&block, &mut decrypted_block);
         plaintext.extend_from_slice(&mut xor(&prev_block, &mut decrypted_block));
         prev_block = block.to_vec();
     }
+
     remove_pkcs5_padding(&plaintext)
 }
 
+/// Encrypts bytes of plaintext using CTR mode and the AES cipher. This implementation uses
+/// a simple counter mode where the IV is incremented sequentially. The Final output
+/// contains the ciphertext with the IV prepended. Note that key should be passes as an
+/// unencoded slice of bytes.
 pub fn aes_128_ctr_encrypt(plaintext: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
     let cipher = AesSafe128Encryptor::new(key);
 
     let mut buffer = vec![];
+
+    // We use the IV as a counter which is incremented after every block
     let mut counter = iv.to_vec();
-    for block in plaintext.chunks(AES_BLOCK_SIZE) {
+
+    for block in plaintext.chunks_exact(AES_BLOCK_SIZE) {
         let mut encrypted_block = [0; AES_BLOCK_SIZE];
         cipher.encrypt_block(&counter, &mut encrypted_block);
         buffer.extend_from_slice(&xor(block, &encrypted_block));
 
         increment_counter(&mut counter);
     }
+
     let mut ciphertext = iv.to_vec();
     ciphertext.append(&mut buffer);
     ciphertext
 }
 
+/// Decrypts bytes of a ciphertext using CTR mode and the AES cipher. It's important
+/// to note that the ciphertext and key should be passes as unencoded byte slices. This
+/// implementation knows how to extract the IV from the ciphertext, which is assumed from
+/// the first 16 bytes of the ciphertext.
 pub fn aes_128_ctr_decrypt(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
     let cipher = AesSafe128Encryptor::new(key);
 
@@ -64,7 +90,7 @@ pub fn aes_128_ctr_decrypt(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
     let mut counter = (&ciphertext[0..AES_BLOCK_SIZE]).to_vec();
     let mut encrypted_block = [0; AES_BLOCK_SIZE];
 
-    for block in ciphertext[AES_BLOCK_SIZE..].chunks(AES_BLOCK_SIZE) {
+    for block in ciphertext[AES_BLOCK_SIZE..].chunks_exact(AES_BLOCK_SIZE) {
         cipher.encrypt_block(&counter, &mut encrypted_block);
         plaintext.extend_from_slice(&xor(block, &encrypted_block));
 
